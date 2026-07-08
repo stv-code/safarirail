50cea3ffda0ea41f9591d5d60fd073caa3f54dde
stv-code <stv-code@users.noreply.github.com>
2026-07-09 00:22:34 +0300

    Rebuild SafariRail site with Astro

--- sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { reviewType } from './reviewType'
-import { faqType } from './faqType'
-import { siteSettingsType } from './siteSettingsType'
-import { pageContentType } from './pageContentType'
-
-export const schemaTypes = [pageContentType, siteSettingsType, reviewType, faqType]
