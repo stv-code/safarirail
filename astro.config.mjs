97e117f204978aa2883adcf4f3c804215845d0fc
stv-code <stv-code@users.noreply.github.com>
2026-07-11 18:07:41 +0300

    Add booking confirmation pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://safarirail.co.ke',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => !page.endsWith('/booking-request-received/') && !page.endsWith('/booking-confirmed/'),
+    }),
+  ],
 })
 
